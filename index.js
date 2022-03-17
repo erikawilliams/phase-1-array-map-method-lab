c98ecf43b5b2502c127e2d87fb72bd966b2930e6
Erika Williams <erika@erikawilliams.us>
2022-03-17 08:16:39 -0600

    commit tree be32c4a9da714d5d70083ff0259858b042168ddc

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+const practiceString = "This is a practice string."
 
 const titleCased = () => {
-  return tutorials
+  const newTut = tutorials.map(caseFunc)
+  console.log(newTut)
+  return newTut
 }
+
+function caseFunc(element){
+    return (element.charAt(0).toUpperCase() +element.slice(1))
+  }
+
+titleCased()
+
+// titleCased = takes in no arguments, outputs a new array with proper casing
+// map tutorials to apply casing function  = input tutorials array and case function, output new final array
+// define case function: for each element:
+//      split by space, capitalize first letter, re-join, add to new object
+
